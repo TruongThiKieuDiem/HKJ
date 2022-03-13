f3a40027974b4a87de2a6c6caacccb8bdf6912b1
kieudiem44K14 <78086594+kieudiem44K14@users.noreply.github.com>
2022-03-13 23:37:33 +0700

    test automation diem dep gai

--- cypress/integration/TC01_login.spec.js
+++ cypress/integration/TC01_login.spec.js
@@ -5,40 +5,40 @@ describe('Hack Kanji BE - Login', function(){
     })
     it ('check font size, corlor of title',function(){
         cy.get('.font-medium')
-            .should('be.visible')
-            .and('have.text','Đăng nhập vào tài khoản')
+            .should('be.visible') //kiem tra ton tai tieu de cua tr
+            .and('have.text','Đăng nhập vào tài khoản') //kiem tra text cua tieu de
     })
     it ('check font size, corlor of title lable username',function(){
         cy.get(':nth-child(2) > .text-gray-light')
-            .should('be.visible')
-            .and('have.text','Tên đăng nhập')
+            .should('be.visible') //kiem tra lable ton tai
+            .and('have.text','Tên đăng nhập') //kiem tra text lable username
     })
     it ('check font size, corlor of title lable password',function(){
-        cy.get(':nth-child(3) > .text-gray-light')
-            .should('be.visible')
-            .and('have.text','Mật khẩu')
+        cy.get(':nth-child(3) > .text-gray-light') 
+            .should('be.visible') //kiem tra label ton tai
+            .and('have.text','Mật khẩu') //kiem tra text lable password
     })
     it ('validate password empty', function(){
-        cy.get(':nth-child(2) > .undefined').type('superadmin3');
-        cy.get(':nth-child(3) > .undefined').type('{alt}');
-        cy.get('.mt-6 > .w-full').click();
+        cy.get(':nth-child(2) > .undefined').type('superadmin3'); //nhap vao textbox username: superadmin3
+        cy.get(':nth-child(3) > .undefined').type('{alt}'); //tro toi textbox password: khong nhap
+        cy.get('.mt-6 > .w-full').click(); //click vao button dang nhap
          
-        cy.get('.text-red-600').should('have.text','*Yêu cầu mật khẩu')
+        cy.get('.text-red-600').should('have.text','*Yêu cầu mật khẩu') //kiem tra thong bao loi o textbox password
     })
     it ('validate username empty', function(){
-        cy.get(':nth-child(2) > .undefined').type('{alt}');
-        cy.get(':nth-child(3) > .undefined').type('Abc123!@#');
-        cy.get('.mt-6 > .w-full').click();
+        cy.get(':nth-child(2) > .undefined').type('{alt}'); //tro toi textbox username: khong nhap
+        cy.get(':nth-child(3) > .undefined').type('Abc123!@#'); //nhap vao textbox password: Abc123!@#
+        cy.get('.mt-6 > .w-full').click(); //click vao button dang nhap
          
-        cy.get('.text-red-600').should('have.text','*Yêu cầu tên đăng nhập')
+        cy.get('.text-red-600').should('have.text','*Yêu cầu tên đăng nhập') //kiem tra thong bao loi o textbox username
     })
     it ('validate username and password empty', function(){
-        cy.get(':nth-child(2) > .undefined').type('{alt}');
-        cy.get(':nth-child(3) > .undefined').type('{alt}');
-        cy.get('.mt-6 > .w-full').click();
+        cy.get(':nth-child(2) > .undefined').type('{alt}'); //tro toi textbox username: khong nhap
+        cy.get(':nth-child(3) > .undefined').type('{alt}'); //tro toi textbox password: khong nhap
+        cy.get('.mt-6 > .w-full').click(); //click vao button dang nhap
          
-        cy.get(':nth-child(2) > .text-red-600').should('have.text','*Yêu cầu tên đăng nhập')
-        cy.get(':nth-child(3) > .text-red-600').should('have.text','*Yêu cầu mật khẩu')
+        cy.get(':nth-child(2) > .text-red-600').should('have.text','*Yêu cầu tên đăng nhập') //kiem tra thong bao loi o textbox username
+        cy.get(':nth-child(3) > .text-red-600').should('have.text','*Yêu cầu mật khẩu') //kiem tra thong bao loi o textbox password
     })
 
 })
