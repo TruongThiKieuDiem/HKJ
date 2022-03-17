51a282d4c6147991b396daeb74a67224f6864083
kieudiem44K14 <78086594+kieudiem44K14@users.noreply.github.com>
2022-03-17 14:33:31 +0700

    test case accountManagement02

---
+++ cypress/integration/accountManagement02.spec.js
@@ -0,0 +1,42 @@
+describe('Account Administrator Management', () => {
+    before(() => {
+        cy.visit('http://115.79.60.98:3000/login')
+        cy.get(':nth-child(2) > .undefined').type('superadmin3');
+        cy.get(':nth-child(3) > .undefined').type('Abc123!@#');
+        cy.get('.mt-6 > .w-full').click();
+
+        cy.wait(6000)
+
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            // returning false here prevents Cypress from
+            // failing the test
+            return false
+          })
+
+        // cy.get('.cursor-pointer > .text-base').click()
+        cy.get('[href="/administrators"] > .h-10').click()
+        
+        cy.wait(3000)
+
+        cy.get('.w-12').click()
+    })
+    it('validate format email valid', function(){
+        cy.get(':nth-child(1) > .undefined').type('test');
+        cy.get(':nth-child(2) > .undefined').type('2');
+        cy.get(':nth-child(3) > .undefined').type('kieudiemttkd1006@gmail.com');
+        cy.get(':nth-child(4) > .undefined').type('test1');
+        cy.get('.rounded-tl-none').type('023447672');
+        cy.get('form > :nth-child(6) > .undefined').type('Test');
+        cy.get('#headlessui-switch-28').click();
+        cy.get('#headlessui-listbox-button-29 > .block').click();
+        cy.get('#headlessui-listbox-option-35 > .font-normal').click();
+        cy.get(':nth-child(9) > .undefined').type('123456');
+
+        cy.get('.px-8').click();
+    })
+    it ('Success Message', () => {
+        const alert = cy.get('.Toastify')
+        const expectedAlert = 'Tạo thành công!'
+        alert.should('contain.text', expectedAlert)
+    });
+})
